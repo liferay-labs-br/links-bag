7b13909bf43dcca517f83b7b45d806c28d9b7d9b
Matuzalém Teles <matuzalemteles@gmail.com>
2018-07-20 19:53:56 -0300

    Hide temporary window

--- main.js
+++ main.js
@@ -151,7 +151,9 @@ function _getTrayIcon() {
 // Some APIs can only be used after this event occurs.
 app.on('ready', () => {
 	createTray();
-	createWindow();
+
+	// Hide temporary window
+	// createWindow();
 });
 
 // Quit when all windows are closed.
@@ -170,6 +172,9 @@ app.on('activate', function () {
 		createWindow();
 	}
 });
+
+// Temporarily hide the dock app, leaving only the notification menu.
+app.dock.hide();
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
