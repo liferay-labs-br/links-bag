c9d6652fb9741baf78e0ac12503cd79a28536d79
Matuzalém Teles <matuzalemteles@gmail.com>
2018-07-20 16:30:24 -0300

    Setup 🎉

--- main.js
+++ main.js
@@ -1,19 +1,61 @@
 // Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron')
+const path = require('path')
+const url = require('url')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
+// Keep a reference for dev mode
+let dev = false
+
+if (process.defaultApp || /[\\/]electron-prebuilt[\\/]/.test(process.execPath) || /[\\/]electron[\\/]/.test(process.execPath)) {
+  dev = true
+}
+
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow(
+    {
+      width: 800, 
+      height: 600,
+      frame: false,
+      titleBarStyle: 'hiddenInset'
+    }
+  )
+
+  let indexPath
+
+  if (dev && process.argv.indexOf('--noDevServer') === -1) {
+    indexPath = url.format({
+      protocol: 'http:',
+      host: 'localhost:8080',
+      pathname: 'index.html',
+      slashes: true
+    })
+  } else {
+    indexPath = url.format({
+      protocol: 'file:',
+      pathname: path.join(__dirname, 'dist', 'index.html'),
+      slashes: true
+    })
+  }
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadURL(indexPath)
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
+  // Don't show until we are ready and loaded
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show()
+
+    // Open the DevTools automatically if developing
+    if (dev) {
+      mainWindow.webContents.openDevTools()
+    }
+  })
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import JSXComponent from 'metal-jsx';
+
+class App extends JSXComponent {
+    render() {
+        return (
+            <div>Hello World!</div>
+        );
+    }
+}
+
+export default App;
