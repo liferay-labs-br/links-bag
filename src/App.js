4826d83970355925e2d0a2e4dfbcc3554a5a4d19
Adriano <adriano.interaminense@gmail.com>
2018-07-21 00:11:52 -0300

    Use Layout component on App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import JSXComponent from 'metal-jsx';
+import Component from 'metal-jsx';
+import Layout from './components/Layout';
+import Links from './components/Links';
 import './style/main.scss';
 
-class App extends JSXComponent {
+class App extends Component {
 	render() {
 		return (
-			<div>Hello World!</div>
+			<Layout>
+				<Layout.Header />
+
+				<Layout.Main>
+				</Layout.Main>
+
+				<Layout.Footer elementClasses={'d-flex justify-content-between'}>
+					<div>{'version 0.0.1'}</div>
+					<div>{'Made with ️❤ on Friday Labs'}</div>
+				</Layout.Footer>
+			</Layout>
 		);
 	}
 }
